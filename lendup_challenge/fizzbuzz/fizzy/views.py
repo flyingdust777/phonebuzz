a62fb06a0373f08ed67bca25eec83073ff510b3d
Gautham <gautham.bommannan@gmail.com>
2017-12-28 18:33:52 -0800

    added some comments

--- lendup_challenge/fizzbuzz/fizzy/views.py
+++ lendup_challenge/fizzbuzz/fizzy/views.py
@@ -15,14 +15,14 @@ from twilio.rest import Client
 
 import os
 
-account_sid = "account_sid" #twilio account_sid
-auth_token = "auth_token" #twilio auth_token
-originNumber = "WHITELISTED PHONE NUMBER HERE" #EX: 6508522194
+account_sid = "ACCOUNT_SID" #twilio account_sid
+auth_token = "AUTH_TOKEN" #twilio auth_token
+originNumber = "WHITELISTED_PHONE_NUMBER" #EX: 6508522194
 targetURL = "http://893f543c.ngrok.io" #put hosted url here
 client = Client(account_sid, auth_token)
 
 
-def validate_twilio_request(f):
+def validate_twilio_request(f): # a wrapper to validate twilio requests
     """Validates that incoming requests genuinely originated from Twilio"""
     @wraps(f)
     def decorated_function(request, *args, **kwargs):
@@ -65,20 +65,21 @@ def index(request):
     # 	return HttpResponse(str(resp), content_type='text/xml')
     # with resp.gather(input='dtmf',finishOnKey='*') as g:
     # 	g.say("Please enter a number to play followed by a star.")
-    return render(request,'index.html')
+    return render(request,'index.html') #load the home page
     # return HttpResponse(str(resp), content_type='text/xml')
 
 @csrf_exempt
-def makeCall(request):
+def makeCall(request): # process to initiate a seperate thread to make a phone call
+	# grab the telephone and delay number
 	phoneNumber = request.POST['telNo']
 	delay = request.POST['delay']
-	thread = Thread(target=newThread,args=(phoneNumber,delay, ))
-	thread.start()
+	thread = Thread(target=newThread,args=(phoneNumber,delay, )) #create a new thread
+	thread.start() #start that new thread
 
-	return render(request,'index.html')
+	return render(request,'index.html') #redirect back to home page, ideally would have a wait page, but there isn't one
 
-def newThread(phoneNumber,delay):
-	time.sleep(int(delay))
+def newThread(phoneNumber,delay): # the new thread that will handle the delay and initiating the outbound call
+	time.sleep(int(delay)) # sleep, so this acts as a delay
 
 	# [account_sid, auth_token, twilio_number] = open('config.txt','r').readlines()
 	# account_sid = account_sid[0:len(account_sid) - 1]
@@ -96,9 +97,10 @@ def newThread(phoneNumber,delay):
 @csrf_exempt
 @validate_twilio_request  #validates twilio function header
 def handleCall(request):
-	number = request.POST.get('Digits')
-	resp = VoiceResponse()
-	if number is not None:
+	number = request.POST.get('Digits') #grab the digits
+	resp = VoiceResponse() #initiate a voice response
+	if number is not None: #check if any digits entered
+		#create the string to say
 		fizzString = ''
 		for x in range(1, int(number) + 1):
 			if x % 15 == 0:
@@ -109,18 +111,18 @@ def handleCall(request):
 				fizzString += 'Fizz, '
 			else:
 				fizzString += str(x) + ', '
-		resp.say(fizzString)
-		return HttpResponse(str(resp), content_type='text/xml')
-	with resp.gather(input='dtmf',finishOnKey='*') as g:
+		resp.say(fizzString) #say the string
+		return HttpResponse(str(resp), content_type='text/xml') #return the TWIML code
+	with resp.gather(input='dtmf',finishOnKey='*') as g: #otherwise, ask for digits (#smooth)
 		g.say("Please enter a number to play followed by a star.")
-	return HttpResponse(str(resp), content_type='text/xml')
+	return HttpResponse(str(resp), content_type='text/xml') #return TWIML xml code
 
 @require_POST
 @csrf_exempt
 @validate_twilio_request  	#allows the user to still call twilio number directly to play the game
-def phase1(request):
-    resp = VoiceResponse()
-    number = request.POST.get('Digits')
+def phase1(request): #the first phase, which allows someone to call, will redirect twilio here to find TWIML code
+    resp = VoiceResponse() #create voice response
+    number = request.POST.get('Digits') #grab digits and everything, same as above function
     if number is not None:
     	fizzString = ''
     	for x in range(1, int(number) + 1):
